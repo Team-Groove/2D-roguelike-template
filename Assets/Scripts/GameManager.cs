c884d4534ab35dbbde829736b46a8acaac1147c9
Matias Lecaros <m.lecaros.aranis@gmail.com>
2021-07-20 23:29:56 -0400

    completed level generation, Board and Game managers and loader to handle singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if(instance != this)
+                Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
